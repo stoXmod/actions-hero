eae1d40b8ef34d296b6fecb0a0d6d25b6d6cc637
stoXmod <nisharasenadheera2@gmail.com>
2023-11-05 09:10:59 -0800

    AH-3 feat: add ci workflow file

--- web/src/app.ts
+++ web/src/app.ts
@@ -6,6 +6,5 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Metaroon DevOps Example ✌');
 });
 
-app.listen(5000, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:5000`);
-});
+export default app
+
