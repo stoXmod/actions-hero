719e643edae7f12d08e402b24f9dfd334048f4a7
stoXmod <nisharasenadheera2@gmail.com>
2023-11-12 04:33:49 -0800

    fix: made some improvements in yaml file

--- infra/instances/staging/output.tf
+++ infra/instances/staging/output.tf
@@ -1,4 +1,7 @@
-output "public_domain_name" {
-  value = "${cloudflare_record.my_instance_dns.name}.${data.cloudflare_zones.my_zone.zones[0].name}"
-  description = "The FQDN of the DNS record"
+output "dns_details" {
+  value = {
+    domain_name = "${cloudflare_record.my_instance_dns.name}.${data.cloudflare_zones.my_zone.zones[0].name}"
+    public_ip = google_compute_instance.staging_pr_demo.network_interface[0].access_config[0].nat_ip
+  }
+  description = "Map containing the DNS record name and zone name"
 }
