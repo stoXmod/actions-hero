b98e7169817e83cc2989ead8d211968848d4f34c
stoXmod <nisharasenadheera2@gmail.com>
2023-11-11 20:35:26 -0800

    feat: integrate cloudflare dns to Terraform #2

--- infra/instances/staging/backend.tf
+++ infra/instances/staging/backend.tf
@@ -12,6 +12,10 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 3.5"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
   }
 
   required_version = ">= 0.15.0"
